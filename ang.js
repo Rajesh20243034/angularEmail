faa2063294149dbc9af5c207ceff28a0f1899b16
Rajesh patel <rajesh.patelp3034@gmail.com>
2015-02-20 17:16:52 +0530

    Email classified in two parts

--- ang.js
+++ ang.js
@@ -1,5 +1,13 @@
 function EmailController($scope) {
 	$scope.isPopUpVisible = false;
+	$scope.composeEmail = {};
+	$scope.activeTab = 'inbox';
+	$scope.sentEmails = [];
+		$scope.isComposePopUpVisible = false;
+	  $scope.sendEmail = function() {
+        $scope.isComposePopUpVisible = false;
+        $scope.sentEmails.push($scope.composeEmail);
+    };
 	$scope.showPopUp = function(email) {
 		$scope.isPopUpVisible = true;
 		$scope.selectedEmail = email;
@@ -7,6 +15,15 @@ function EmailController($scope) {
 	$scope.closePopUp = function() {
 		$scope.isPopUpVisible = false;
 	};
+
+
+	$scope.showComposePopUp = function() {
+		$scope.composeEmail = {};
+		$scope.isComposePopUpVisible = true;
+	};
+	$scope.closeComposePopUp = function() {
+		$scope.isComposePopUpVisible = false;
+	}
 
 	$scope.emails = [{from:'john', subject:'I love Angular', date:'jan1', body:'Hello World'},
         			{from:'jack', subject:'Angular and I are Just Friends', date:'02jan', body:'Just Kidding'},
--- index.php
+++ index.php
@@ -11,9 +11,13 @@
 </head>
 <body>
     <div class="container" ng-app ng-controller="EmailController">
+      <ul class="nav nav-tabs">
+        <li ng-class="{active: activeTab == 'inbox'}"><a ng-click="activeTab='inbox'">Inbox</a></li>
+        <li ng-class="{active: activeTab == 'sent'}"><a ng-click="activeTab='sent'"> Sent</a></li>
+      </ul>
       <table class="table table-bordered table-condensed">
         <tbody>
-            <tr ng-repeat="email in emails" ng-click="showPopUp(email)">
+            <tr ng-show="activeTab=='inbox'" ng-repeat="email in emails" ng-click="showPopUp(email)">
               <td>
                 {{email.from}}
               </td>
@@ -29,6 +33,17 @@
             </tr>
         </tbody>
       </table>
+       <table class="table table-bordered table-condensed" ng-show="activeTab=='sent'">
+        <tbody>
+            <tr ng-repeat="email in sentEmails" ng-click="showPopUp(email)">
+              <td>{{ email.to }}</td>
+                <td>{{ email.subject }}</td>
+                <td>{{ email.date }}</td>
+            </tr>
+        </tbody>
+      </table>
+          <button class="btn btn-primary" ng-click="showComposePopUp()">Compose</button>
+
       <div class="modal" ng-show="isPopUpVisible">
       <div class="modal-header">
           <button type="button" class="close" ng-click="closePopUp()">×</button>
@@ -44,6 +59,23 @@
       </div>
       <div class="modal-footer">
           <a href="#" class="btn btn-primary" ng-click="closePopUp()">Close</a>
+      </div>
+    </div>
+    <div class="modal" ng-show="isComposePopUpVisible">
+      <div class="modal-header">
+          <button type="button" class="close" ng-click="closeComposePopUp()">×</button>
+          <h3>Compose Email</h3>
+      </div>
+      <div class="modal-body">
+          <form>
+              <input type="text" placeholder="To" style="width:95%;" ng-model="composeEmail.to"><br />
+              <input type="text" placeholder="Subject" style="width:95%;" ng-model="composeEmail.subject"><br />
+              <textarea style="width:95%;" rows="10" ng-model="composeEmail.body"></textarea>
+          </form>
+      </div>
+      <div class="modal-footer">
+          <a href="#" class="btn" ng-click="closeComposePopUp()">Close</a>
+          <a href="#" class="btn btn-primary" ng-click="sendEmail() ">Send</a>
       </div>
     </div>
     </div>
